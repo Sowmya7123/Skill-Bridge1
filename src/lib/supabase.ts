fd8d8727aecbe8566e031480734f597df716dac5
Sowmya7123 <kslsowmya7123@gmail.com>
2026-09-20 12:04:02 +0530

    fix superbase anon key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const SUPABASE_URL = "https://yxmgiwiwsvcrjyhteqzq.supabase.co";
-export const SUPABASE_ANON_KEY = "sb_publishable_6B9UOtASWAzc2jvPuOrskg_xWoFObEB";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl4bWdpd2l3c3Zjcmp5aHRlcXpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk2Mjk0NzYsImV4cCI6MjEwNTIwNTQ3Nn0.F3MbK8q77Jeiy4x5niM6amXuPyiD5TkE5cQe76x1XsQ";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
